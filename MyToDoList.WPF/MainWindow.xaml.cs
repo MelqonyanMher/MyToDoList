4a4743b2ca9b66ddd814f2d06e4229463ba230c2
mic.armenia <mic.armenia@hotmail.com>
2019-04-27 18:48:40 +0400

    Add btn DeleteCompleated

--- MyToDoList.WPF/MainWindow.xaml.cs
+++ MyToDoList.WPF/MainWindow.xaml.cs
@@ -27,17 +27,31 @@ namespace MyToDoList.WPF
 
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
-            if( listBoxToDoListBox.Height <300)
-                       listBoxToDoListBox.Height += 51;
+            if (listBoxToDoListBox.Height < 300)
+                listBoxToDoListBox.Height += 51;
 
             listBoxToDoListBox.Items.Add(new TaskItam(textBoxAddTask.Text));
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-            
+
             listBoxToDoListBox.Items.Remove(listBoxToDoListBox.SelectedItem);
-         
+
+        }
+
+        private void btnDeleteCampleated_Click(object sender, RoutedEventArgs e)
+        {
+            var itm = listBoxToDoListBox.Items;
+            for (int i = 0; i < itm.Count; i++)
+            {
+                TaskItam ti = (itm.GetItemAt(i) as TaskItam);
+                 
+                if ((bool)(ti).checkBoxCompleated.IsChecked)
+                {
+                    listBoxToDoListBox.Items.Remove(ti);
+                }
+            }
         }
     }
 }
