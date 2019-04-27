e208a4a199800ea70080463728be9a8bdfcc61c7
mic.armenia <mic.armenia@hotmail.com>
2019-04-27 15:45:18 +0400

    Add userControl

---
+++ MyToDoList.WPF/TaskItam.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyToDoList.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskItam.xaml
+    /// </summary>
+    public partial class TaskItam : UserControl
+    {
+        public TaskItam()
+        {
+            InitializeComponent();
+        }
+    }
+}
