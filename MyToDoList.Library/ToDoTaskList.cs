95a53bd640a0a775a9f074aecdeae6583158f838
mic.armenia <mic.armenia@hotmail.com>
2019-04-27 15:31:25 +0400

    Add ToDoListTask class

--- MyToDoList.Library/ToDoTaskList.cs
+++ MyToDoList.Library/ToDoTaskList.cs
@@ -8,5 +8,81 @@ namespace MyToDoList.Library
 {
     public class ToDoTaskList
     {
+        private class MyToDo
+        {
+            public MyToDo(string value)
+            {
+                Value = value;
+            }
+
+            public string Value { get; set; }
+            public bool Completed { get; set; } = false;
+        }
+
+        private List<MyToDo> myList;
+
+        public bool IsEmpty => myList == null;
+
+        public void Add(string s)
+        {
+            if (IsEmpty)
+            {
+                myList = new List<MyToDo>();
+                myList.Add(new MyToDo(s));
+            }
+            else
+            {
+                myList.Add(new MyToDo(s));
+            }
+        }
+
+        public void Remove(string s)
+        {
+            if (IsEmpty)
+            {
+                throw new NullReferenceException("List is Empty!");
+            }
+
+            MyToDo m = null;
+
+            foreach (var item in myList)
+            {
+                if (item.Value == s)
+                {
+                    m = item;
+                }
+                else throw new Exception("string wasn't found!");
+
+            }
+            myList.Remove(m);
+
+        }
+
+        public void RemoveAll(bool b)
+        {
+            if (IsEmpty)
+            {
+                throw new NullReferenceException("List is Empty!");
+            }
+            myList.RemoveAll(x => x.Completed == b);
+        }
+
+        public IEnumerable<string> GetAll()
+        {
+            if (IsEmpty)
+            {
+                throw new NullReferenceException("List is Empty!");
+            }
+            return myList.Select(x => x.Value);
+        }
+
+        public IEnumerable<string> GetAll(bool b)
+        {
+            if (IsEmpty)
+            {
+                throw new NullReferenceException("List is Empty!");
+            }
+            return myList.Where(x => x.Completed == b).Select(x => x.Value);
+        }
     }
 }
