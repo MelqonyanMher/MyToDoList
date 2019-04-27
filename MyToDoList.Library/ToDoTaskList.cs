e3adc20e4daac7f64bc6440d5d231d1412105709
mic.armenia <mic.armenia@hotmail.com>
2019-04-27 15:34:42 +0400

    fix todo itam

--- MyToDoList.Library/ToDoTaskList.cs
+++ MyToDoList.Library/ToDoTaskList.cs
@@ -15,6 +15,7 @@ namespace MyToDoList.Library
                 Value = value;
             }
 
+            public Guid Id { get; set; }
             public string Value { get; set; }
             public bool Completed { get; set; } = false;
         }
